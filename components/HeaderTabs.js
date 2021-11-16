e0b09149610b998615a20d9f3f31a669057354d6
CMGeorges <61295187+CMGeorges@users.noreply.github.com>
2021-11-16 00:14:58 -0500

    :apple: Update HeaderTabs.js :fire:

--- components/HeaderTabs.js
+++ components/HeaderTabs.js
@@ -5,18 +5,21 @@ export default function HeaderTabs() {
     return (
         <View style={{flexDirection:"row",alignSelf:"center"}}>
         {/* Header Button 1 */}
-        <HeaderButton name='Delivery' />
+        <HeaderButton name='Delivery' btnColor="black" textColor="white"/>
         {/* Header Button 2 */}
-        <HeaderButton name='Pick Up' />
+        <HeaderButton name='PickUp' btnColor="white" textColor="black"/>
         </View>
     )
 }
 
 const HeaderButton = (props)=> {
     return (
-    <View>
-        <TouchableOpacity>
-        <Text>{props.name}</Text>
+        <TouchableOpacity style={{
+            backgroundColor: props.btnColor,
+            paddingVertical: 6,
+            paddingHorizontal: 16,
+            borderRadius: 30,
+        }}>
+        <Text style={{ color: props.textColor }}>{props.name}</Text>
         </TouchableOpacity>
-    </View>)
-    };
+    )};
