2e123d0569b37c07126628a1b24e10b780cffbe1
CMGeorges <61295187+CMGeorges@users.noreply.github.com>
2021-12-03 00:36:33 -0500

    :new: Add RestaurantItems.js as new component :100:

---
+++ components/RestaurantItem.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { View, Text,Image } from "react-native";
+
+export default function RestaurantItem() {
+  return (
+    <View>
+      {/* RestaurantImage */}
+      {/* RestaurantInfo */}
+    </View>
+  );
+}
+
+
+const RestaurantImage = () => {
+    <Image
+      source={{ uri: '' }}
+      style={{ width:  , height:  }}
+      PlaceholderContent={}
+    />
+}
--- screens/Home.js
+++ screens/Home.js
@@ -1,19 +1,21 @@
-import React from 'react'
-import { View, Text,SafeAreaView } from 'react-native';
-import Categories from '../components/Categories';
+import React from "react";
+import { View, Text, SafeAreaView, ScrollView } from "react-native";
+import Categories from "../components/Categories";
 import HeaderTabs from "../components/HeaderTabs";
+import RestaurantItem from "../components/RestaurantItem";
 import SearchBar from "../components/SearchBar";
 
 export default function Home() {
-    return (
-        <SafeAreaView style={{ backgroundColor: "#eee" , flex: 1  }} >
-            <View style={{ backgroundColor:"white", padding: 15 }}>
-                <HeaderTabs />
-                <SearchBar />
-            </View>
-
-            <Categories />
-            
-        </SafeAreaView>
-    );
+  return (
+    <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
+      <View style={{ backgroundColor: "white", padding: 15 }}>
+        <HeaderTabs />
+        <SearchBar />
+      </View>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Categories />
+        <RestaurantItem />
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
