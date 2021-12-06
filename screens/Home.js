560bad8367ca9cb98244a86bb9d9ebdd8bd7bd28
CMGeorges <61295187+CMGeorges@users.noreply.github.com>
2021-12-06 16:19:29 -0500

    :speak_no_evil:  Update RestaurantItems.js

--- screens/Home.js
+++ screens/Home.js
@@ -2,10 +2,19 @@ import React from "react";
 import { View, Text, SafeAreaView, ScrollView } from "react-native";
 import Categories from "../components/Categories";
 import HeaderTabs from "../components/HeaderTabs";
-import RestaurantItem from "../components/RestaurantItem";
+import RestaurantItems from "../components/RestaurantItems";
 import SearchBar from "../components/SearchBar";
 
+const YELP_API_KEY =
+  "2txX4O1-3rVaZpwI-zyASNxERFxFTFurip4NiwZWeFD1LyVhsskNfvCcjMWnMbTU-L7LmPQu6T-n7rWEwNeQD75YEymgU2d7l-jxF1Kf4h_TxLMSCnhseMvSQ6SrYXYx";
+
 export default function Home() {
+  const [restaurantData, SetRestaurantData] = React.useState([]);
+
+  const getRestaurantsFromYelp = () => {
+    const yelpUrl = "";
+  };
+
   return (
     <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
       <View style={{ backgroundColor: "white", padding: 15 }}>
@@ -14,7 +23,7 @@ export default function Home() {
       </View>
       <ScrollView showsVerticalScrollIndicator={false}>
         <Categories />
-        <RestaurantItem />
+        <RestaurantItems restaurantData={restaurantData} />
       </ScrollView>
     </SafeAreaView>
   );
