3087c55a6787b5b31212378d8c3ead3fb6b20cb4
CMGeorges <61295187+CMGeorges@users.noreply.github.com>
2021-12-10 04:03:26 -0500

    Cors issue for yelp api

--- screens/Home.js
+++ screens/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View, Text, SafeAreaView, ScrollView } from "react-native";
 import Categories from "../components/Categories";
 import HeaderTabs from "../components/HeaderTabs";
@@ -9,11 +9,34 @@ const YELP_API_KEY =
   "2txX4O1-3rVaZpwI-zyASNxERFxFTFurip4NiwZWeFD1LyVhsskNfvCcjMWnMbTU-L7LmPQu6T-n7rWEwNeQD75YEymgU2d7l-jxF1Kf4h_TxLMSCnhseMvSQ6SrYXYx";
 
 export default function Home() {
-  const [restaurantData, SetRestaurantData] = React.useState([]);
+  const [restaurantData, setRestaurantData] = React.useState([]);
 
   const getRestaurantsFromYelp = () => {
-    const yelpUrl = "";
+    const yelpUrl =
+      "https://api.yelp.com/v3/businesses/search?term=restaurants&location=Hollywood";
+
+    const apiOptions = {
+      //TODO: CORS Issues
+      //mode: "no-cors",
+      //Origin: ["http://localhost:19006/"],
+      headers: {
+        //"Access-Control-Allow-Origin": "*",
+        Authorization: `Bearer ${YELP_API_KEY}`,
+      },
+    };
+
+    return fetch(yelpUrl, apiOptions)
+      .then((res) => {
+        res.json();
+      })
+      .then((json) => {
+        setRestaurantData(json.businesses);
+      });
   };
+
+  useEffect(() => {
+    getRestaurantsFromYelp();
+  }, []);
 
   return (
     <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
