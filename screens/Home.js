e3fbc2219e5aa0ba7116030b75ef2dc6d5aacfa5
CMGeorges <61295187+CMGeorges@users.noreply.github.com>
2022-01-04 00:56:01 -0500

    :ambulance: Issues filter response,,,

--- screens/Home.js
+++ screens/Home.js
@@ -9,28 +9,32 @@ import SearchBar from "../components/SearchBar";
 
 export default function Home() {
   const [restaurantData, setRestaurantData] = useState([]);
-  const [searchInput, setSearchInput] = useState("Montréal");
+  const [searchInput, setSearchInput] = useState("Montreal");
   const [activeTab, setActiveTab] = useState("Delivery");
   const [errorMessage, setErrorMessage] = useState("");
 
   const getRestaurantsFromYelp = async () => {
     try {
-      const response = await yelp.get("/search", {
-        params: {
-          limit: 75,
-          term: "restaurants",
-          location: searchInput,
-        },
-      });
-      setRestaurantData(
-        response.data.businesses
-        // .filter((business) =>
-        //   business.transactions.includes(activeTab.toLowerCase())
-        // )
-      );
-      setErrorMessage("");
+      return await yelp
+        .get("/search", {
+          params: {
+            limit: 50,
+            term: "restaurants",
+            location: searchInput,
+          },
+        })
+        .then((response) => {
+          console.log(response.data.businesses);//filter doesn't work....
+          setRestaurantData(response.data.businesses
+            );
+        }).then(json => {
+          console.log(json);
+        });
+        
+        setErrorMessage("");
     } catch (err) {
       setErrorMessage("No restaurants");
+      //console.log(err);
     }
   };
 
